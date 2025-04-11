a6d50b959f0d4dce6017cdbd6de1e9850c87cbc2
lubkz <letreiroomega@gmail.com>
2025-04-10 22:35:43 -0300

    Substituindo projeto Django Flask

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import ferramentas.ChatBotFunc as ChatBot
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template("index.html")
+
+@app.route("/chatbot", methods=["GET", "POST"])
+def ChatBotResposta():
+    resposta = None
+    if request.method == "POST":
+        pergunta = request.form['pergunta']
+        resposta = ChatBot.encontrar_resposta(pergunta=pergunta)
+    return render_template("chatbot.html", resposta=resposta)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0,0,0,0", port=8000)
---
+++ static/javascript/main.js
@@ -0,0 +1,142 @@
+function minhaFuncao() {
+    alert("Você clicou!")
+
+}
+
+function ativarCorrupcao() {
+    const elementos = document.querySelectorAll("h1, h2, p, a"); // você pode adicionar outros se quiser
+    const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:<>,.?/";
+    let intervalos = [];
+
+    let glitchCriado = false;
+
+    const textosOriginais = new Map();
+
+    let somGlit = document.getElementById("glitchSound")
+    somGlit.volume = 0.2;
+    let somTrn = document.getElementById("alghSound")
+    somTrn.volume = 0.3;
+
+    somGlit.play();
+    somTrn.play();
+
+    document.body.style.backgroundColor = "#000000"
+    
+    elementos.forEach(el => {
+      const textoOriginal = el.innerText;
+      textosOriginais.set(el, textoOriginal)
+      let i = 0;
+  
+      const intervalo = setInterval(() => {
+        let spreadBtn = document.querySelector('button[onclick="ativarCorrupcao()"]');
+        spreadBtn.disabled = true;
+
+        let textoCorrompido = "";
+        for (let j = 0; j < textoOriginal.length; j++) {
+          if (textoOriginal[j] === " ") {
+            textoCorrompido += " ";
+          } else {
+            textoCorrompido += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+          }
+        }
+        el.innerText = textoCorrompido;
+        i++;
+        
+
+        // Para após algumas "corrupções"
+        if (i > 65) {
+          clearInterval(intervalo);
+
+          if (!glitchCriado) {
+            glitchCriado = true
+          
+            let voice = document.createElement("p");
+            voice.style.position = "fixed";
+            voice.style.bottom = "20px";
+            voice.style.left = "50%";
+            voice.style.transform = "translateX(-50%)"
+            voice.style.color ="rgb(255, 0, 0)"
+
+            voice.textContent = "__Null _intent";
+            document.body.appendChild(voice);
+
+            setTimeout(() => {
+              console.log(voice.innerText = "__Behind _good _Int--t-s")
+
+              setTimeout(() => {
+                let btn = document.createElement("button")
+                btn.textContent = "Not yours"
+                btn.style.position = "fixed";
+                btn.style.bottom = "50px";
+                btn.style.right = "50px"
+                document.body.appendChild(btn)
+
+                btn.onclick = () => {
+                  let rect = btn.getBoundingClientRect();
+                  let olho = mostrarOlho();
+                  spreadBtn.disabled = false;
+
+                  voice.remove();
+
+                  document.body.style.color = "black"
+
+                  somGlit.play();
+
+                  document.getElementById("botaoSpread").classList.add("escondido")
+                  document.getElementById("botaoChatBot").classList.add("escondido") 
+                  document.getElementById("navBarInicio").classList.add("escondido")
+                  
+
+                  olho.style.width = btn.offsetWidth + "px"
+                    // Aplica as mesmas dimensões e posição ao olho
+                  olho.style.position = "fixed"; // importante!
+                  olho.style.top = rect.top + "px";
+                  olho.style.left = rect.left + "px";
+                  olho.style.width = rect.width + "px";
+
+                  btn.replaceWith(olho);
+
+                  setTimeout(() => {
+                    document.body.style.backgroundColor = "#f5f5f5";
+
+                    somGlit.play();
+
+                    textosOriginais.forEach((texto, elemento) => {
+                      elemento.innerText = texto;
+
+                    })
+
+                    document.body.style.color = "#333"
+
+                    document.getElementById("botaoSpread").classList.remove("escondido")
+                    document.getElementById("botaoChatBot").classList.remove("escondido") 
+                    document.getElementById("navBarInicio").classList.remove("escondido")
+
+                  }, 3000)
+                }
+              }, 2000);
+            }, 5000)
+          }
+        }
+        }, 100);
+
+      intervalos.push(intervalo);
+    });
+  }
+  
+function mostrarOlho () {
+  let img = document.createElement("img");
+  img.src="static/gifs/olho.gif";
+  img.style.position = "fixed";
+  img.style.top = "50%";
+  img.style.left = "50%";
+  img.style.transform = "translate(-50%, -50%)";
+  img.style.zIndex = "9999";
+  document.body.appendChild(img);
+
+  setTimeout (() => {
+    img.remove();
+  }, 3000);
+
+  return img;
+}
